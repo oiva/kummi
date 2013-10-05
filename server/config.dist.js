55557a59ab46c8d2e30cf110a73ceccc032f1634
Oiva Eskola <oiva.eskola@gmail.com>
2013-10-05 17:02:34 +0300

    placeholder config file

---
+++ server/config.dist.js
@@ -0,0 +1,12 @@
+var config = {
+  user: '', // reittiopas API username & password
+  pass: '',
+
+  api_key: '', // open 311 API key
+
+  db_name: '', // database name / username / password
+  db_user: '',
+  db_passwd: ''
+};
+
+module.exports = config;
