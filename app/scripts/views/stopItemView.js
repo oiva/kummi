0dd3b2c4d88b71254c05b9d85df32a63f382c283
Oiva Eskola <oiva.eskola@gmail.com>
2013-10-29 23:38:02 +0200

    remove unnecessary initialiser for now

--- app/scripts/views/stopItemView.js
+++ app/scripts/views/stopItemView.js
@@ -8,9 +8,6 @@ function(Backbone, Template) {
   
   return Backbone.Marionette.ItemView.extend({
     template: Template,
-    tagName: 'li',
-    initialize: function() {
-      console.log('stop item view init');
-    }
+    tagName: 'li'
   });
 });
