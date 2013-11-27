e39bae1dd43dca24b338c2d19168952c7c74a5ec
Oiva Eskola <oiva.eskola@gmail.com>
2013-11-27 23:28:32 +0200

    add comments to config placeholder

--- server/config.dist.js
+++ server/config.dist.js
@@ -1,15 +1,16 @@
 var config = {
   open311: {
-    endpoint: '',
-    api_key: '' // open 311 API key
+    endpoint: 'https://pate.affecto.com/restWAR/open311/v1', // test API
+    // endpoint: 'https://asiointi.hel.fi/palautews/rest/v1/', // production API
+    api_key: '' // open 311 API key: get one from http://dev.hel.fi/apis/issuereporting
   },
   reittiopas: {
-    host: '',
-    user: '', // reittiopas API username & password
-    pass: ''
+    host: 'api.reittiopas.fi',
+    user: '', // reittiopas API username & password:
+    pass: ''  // get from http://developer.reittiopas.fi/pages/en/account-request.php
   },
 
-  db_name: '', // database name / username / password
+  db_name: '', // mongoDB database name / username / password
   db_user: '',
   db_passwd: ''
 };
