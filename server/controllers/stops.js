f05515509d0f78dcb9711c89d1fc2a9e38e7a063
Oiva Eskola <oiva.eskola@gmail.com>
2014-02-28 19:52:40 +0200

    make stops API cache results fetched from HSL API

--- server/controllers/stops.js
+++ server/controllers/stops.js
@@ -1,4 +1,8 @@
 var http = require('http');
+var cache_manager = require('cache-manager');
+var Q = require('q');
+
+var cache = cache_manager.caching({store: 'memory', max: 1000, ttl: 86400/*seconds*/});
 
 var config = require('../config');
 var Stop = require('../models/stop');
@@ -10,11 +14,44 @@ exports.list = function(req, res) {
 
   var position = wgs84tokkj.WGS84lalo_to_KKJxy(lat, lon);
   var pos = Math.round(position[1])+','+Math.round(position[0]);
+  var cache_key = pos.replace(',', '');
+
+  console.log('stops: request for stops in '+cache_key);
+
+  // check if results is in cache already
+  cache.get(cache_key, function(err, value) {
+    if (!err && value) {
+      console.log('found stops from cache ('+cache_key+')');
+      writeOutput(res, JSON.parse(value));
+    } else {
+      getStopsFromAPI(pos)
+        .then(function(output) {
+          // store results in cache
+          cache.set(cache_key, JSON.stringify(output), function(err) {
+            if (err) {
+              console.log(err);
+            }
+            writeOutput(res, output);
+          });
+        },
+        function(err) {
+          res.send('error: ' + err.message);
+        });
+    }
+  });
+}
+
+var getStopsFromAPI = function(pos) {
+  console.log('getStopsFromAPI: '+pos);
+  var path = '/hsl/prod/?request=stops_area&center_coordinate='+pos;
+  path += '&user='+config.reittiopas.user+'&pass='+config.reittiopas.pass+'&radius=250';
 
   var options = {
     host: config.reittiopas.host,
-    path: '/hsl/prod/?request=stops_area&center_coordinate='+pos+'&user='+config.reittiopas.user+'&pass='+config.reittiopas.pass+'&radius=250'
+    path: path
   };
+
+  var deferred = Q.defer();
 
   var req = http.get(options, function(resp) {
     var output = '';
@@ -25,16 +62,23 @@ exports.list = function(req, res) {
 
     resp.on('end', function() {
       console.log('API call successful');
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.write(output);
-      res.end();
+      deferred.resolve(output);
     });
   });
 
   req.on('error', function(err) {
-    res.send('error: ' + err.message);
+    deferred.reject(err);
   });
-}
+
+  return deferred.promise;
+};
+
+var writeOutput = function(res, output) {
+  console.log('writeOutput');
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write(output);
+  res.end();
+};
 
 exports.show = function(req, res) {
   var code = req.params.code
